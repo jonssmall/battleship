c32c5d256971c30e93facce704dfea23f1955872
Jon Small <jonssmall@gmail.com>
2019-05-06 14:52:10 -0500

    WIP on single player offline UI test

--- web/offline/game.ts
+++ web/offline/game.ts
@@ -1,12 +1,27 @@
 import { newSquareGrid } from "../../game/maker/gridMaker";
+import { Coordinate } from "../../game/models/coordinate";
+import { Cell } from "../../game/models/grid";
+import { playerFactory } from "../../game/models/player";
+import { getCell } from "../../game/utilities/getCell";
 import { gridToBoard } from "../misc/gridToBoard";
 import "./style.css";
 
-const grid = newSquareGrid(10);
-const board = gridToBoard(grid);
+const playerGrid = newSquareGrid(10);
+const botGrid = newSquareGrid(10);
+const player = playerFactory(playerGrid, botGrid);
+const bot = playerFactory(botGrid, playerGrid);
+
+const board = gridToBoard(botGrid);
 
 const app = document.querySelector("#app");
 
 if (app) {
     app.appendChild(board);
+}
+
+// todo: clarity of scope in gridToBoard
+export function discoverCell(coord: Coordinate): Cell {
+    const cell = getCell(botGrid, coord);
+    player.checkSquare(cell);
+    return cell;
 }
