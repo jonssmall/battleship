5a135410e9dd53bb4db6df4da0d6b5dda13682c2
Jon Small <jonssmall@gmail.com>
2019-05-06 14:51:10 -0500

    add click event for coordinates

--- web/misc/gridToBoard.ts
+++ web/misc/gridToBoard.ts
@@ -1,4 +1,6 @@
+import { Coordinate } from "../../game/models/coordinate";
 import { Cell, Grid, Row } from "../../game/models/grid";
+import { discoverCell } from "../offline/game";
 
 export function gridToBoard(grid: Grid): HTMLElement {
     const board = document.createElement("div");
@@ -20,10 +22,33 @@ function rowToDiv(row: Row, index: number): HTMLElement {
 }
 
 function cellToDiv(cell: Cell, index: number): HTMLElement {
-    const uiCell = document.createElement("span");
+    const uiCell = document.createElement("div");
 
     uiCell.setAttribute("data-cell", index.toString());
     uiCell.classList.add("cell");
 
+    uiCell.addEventListener("click", (e) => {
+        const coords = targetToCoords(e.target as HTMLElement);
+        const nextCell = discoverCell(coords);
+
+        if (nextCell.discovered) {
+            if (nextCell.occupiedBy) {
+                uiCell.classList.add("occupied");
+            } else {
+                uiCell.classList.add("discovered");
+            }
+        }
+    });
+
     return uiCell;
+}
+
+function targetToCoords(target: HTMLElement): Coordinate {
+    const x = target.getAttribute("data-cell");
+    const y = target.parentElement.getAttribute("data-row");
+
+    return {
+        x: parseInt(x, 10),
+        y: parseInt(y, 10),
+    };
 }
